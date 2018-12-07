f89c84ea95890dd21943d9d27f21cf981958cf7e
Samantha Hyatt <shhyatt@gmail.com>
2018-12-07 04:22:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+
+def dictionary 
+  shorten_words = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"} 
+end 
+
+def word_substituter(tweet)
+  
